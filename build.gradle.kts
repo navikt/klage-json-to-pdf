e8b3ce3648cb42152cafa12d17bd53d65bc25bf7
Andreas Jonsson <andreas.jonsson@nav.no>
2021-09-23 14:39:06 +0200

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val mockkVersion = "1.9.3"
+val logstashVersion = "5.1"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.72"
+    idea
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation("org.projectreactor:reactor-spring:1.0.1.RELEASE")
+
+    implementation("ch.qos.logback:logback-classic")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.springframework:spring-mock:2.0.8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage")
+    }
+    testImplementation("org.mockito:mockito-inline:2.13.0")
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    this.archiveFileName.set("app.jar")
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src/main/kotlin")
+kotlin.sourceSets["test"].kotlin.srcDirs("src/test/kotlin")
+
+sourceSets["main"].resources.srcDirs("src/main/resources")
+sourceSets["test"].resources.srcDirs("src/test/resources")
---
+++ src/main/kotlin/no/nav/klage/pdfgen/Application.kt
@@ -0,0 +1,12 @@
+package no.nav.klage.pdfgen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [ErrorMvcAutoConfiguration::class])
+class Application
+
+fun main() {
+    runApplication<Application>()
+}
