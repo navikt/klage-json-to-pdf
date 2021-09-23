ccc29c5aaccf570eca751e94de22f60c11a8bffe
Andreas Jonsson <andreas.jonsson@nav.no>
2021-09-23 15:43:29 +0200

    Boilerplate test and fixed action

---
+++ src/test/kotlin/no/nav/klage/pdfgen/GeneratePDF.kt
@@ -0,0 +1,13 @@
+package no.nav.klage.pdfgen
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GeneratePDF {
+
+    @Test
+    fun `test it`() {
+        assertThat("a").isEqualTo("a")
+    }
+
+}
