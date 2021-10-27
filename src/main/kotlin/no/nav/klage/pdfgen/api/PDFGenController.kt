2f945d2cf14d9cba67067b2bbc4e9169f4735aca
Andreas Jonsson <andreas.jonsson@nav.no>
2021-10-27 12:39:28 +0200

    CORS test

--- src/main/kotlin/no/nav/klage/pdfgen/api/PDFGenController.kt
+++ src/main/kotlin/no/nav/klage/pdfgen/api/PDFGenController.kt
@@ -6,12 +6,10 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin
 class PDFGenController(
     private val pdfGenService: PDFGenService
 ) {
